29994c96627793be9916dc07c636449edfa9fd2f
Brian Haas <brianjohnhaas@gmail.com>
2015-08-28 19:54:13 -0400

    updates to SGE runner

--- PerlLib/HPC/SGE_handler.pm
+++ PerlLib/HPC/SGE_handler.pm
@@ -36,8 +36,8 @@ sub submit_job_to_grid {
 
     my $cmd = $self->{config}->{cmd} or confess "Error, need cmd from config file";
     
-    $cmd .= " $shell_script 2>&1 ";
-        
+    $cmd .= " -e $shell_script.stderr -o $shell_script.stdout $shell_script 2>&1 ";
+    
 
     my $job_id_text = `$cmd`;
     #print STDERR "\nSGE: $job_id_text\n";
