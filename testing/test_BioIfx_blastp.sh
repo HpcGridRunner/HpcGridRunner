f37730a10d5ab75b2efec53c7c18ba4d43d5a9ce
Brian Haas <brianjohnhaas@gmail.com>
2018-06-07 14:37:54 -0400

    test works w/ Broad UGE

--- testing/test_BioIfx_blastp.sh
+++ testing/test_BioIfx_blastp.sh
@@ -1,5 +1,5 @@
 ../BioIfx/hpc_FASTA_GridRunner.pl \
-        --cmd_template "blastp -query __QUERY_FILE__ -db /seq/RNASEQ/DBs/SWISSPROT/current/uniprot_sprot.pep  -max_target_seqs 1 -outfmt 6 -evalue 1e-5" \
+        --cmd_template "blastp -query __QUERY_FILE__ -db /seq/RNASEQ/DBs/SWISSPROT/current/uniprot_sprot.fasta  -max_target_seqs 1 -outfmt 6 -evalue 1e-5" \
         --query_fasta `pwd`/test.pep \
-        -G ../hpc_conf/BroadInst_LSF.test.conf \
+        -G ../hpc_conf/BroadInst_UGER.conf  \
         -N 10 -O test_blastp_search
